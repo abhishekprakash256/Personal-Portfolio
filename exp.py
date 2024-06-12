11fb13eaf85a865a39f01057e441e9e7df813842
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-06-12 02:06:47 -0400

    exp code for database added

--- exp.py
+++ exp.py
@@ -22,21 +22,6 @@ messages
         I am good any plans 
 
 
-
-
-"""
-
-
-from datetime import datetime
-chat_hash = "ijGTeB"
-
-user_hash_1 = "InF3ZSI.ZmkriQ.lp1JQX0LsKUsHwcJZBlc-KYlfgk"
-
-user_hash_2 = "InJ0eSI.ZmksAg.t_VuaonAEQYHqy-Bbg4Sqt1NMmM"
-
-message = "hi"
-
-
 message_doc = {
     'chat_hash': chat_hash,
     'sender_hash': user_hash_1,
@@ -45,8 +30,54 @@ message_doc = {
     'timestamp': datetime.now()
 }
 
-print(message_doc)
 
+"""
+from datetime import datetime
+from mongo.mongo_helper import *
+
+
+#database and collection
+DATA_BASE_NAME = "test-chat-data"
+COLLECTION_NAME = "test-chat-message"
+
+
+#make the instance of the class
+mongo_helper_class = Helper_fun()
+
+#make the database and collection 
+mongo_helper_class.make_database_and_collection(DATA_BASE_NAME,COLLECTION_NAME)
+
+
+
+chat_hash = "ijGTeB"
+
+user_hash_1 = "InF3ZSI.ZmkriQ.lp1JQX0LsKUsHwcJZBlc-KYlfgk"
+
+user_hash_2 = "InJ0eSI.ZmksAg.t_VuaonAEQYHqy-Bbg4Sqt1NMmM"
+
+
+
+
+
+
+
+#insert data in message db 
+#mongo_helper_class.insert_message_data(DATA_BASE_NAME,COLLECTION_NAME,chat_hash,user_hash_1,user_hash_2,"hi")
+#mongo_helper_class.insert_message_data(DATA_BASE_NAME,COLLECTION_NAME,chat_hash,user_hash_2,user_hash_1,"how are u")
+#mongo_helper_class.insert_message_data(DATA_BASE_NAME,COLLECTION_NAME,chat_hash,user_hash_1,user_hash_2,"I am good")
+#mongo_helper_class.insert_message_data(DATA_BASE_NAME,COLLECTION_NAME,chat_hash,user_hash_1,user_hash_2,"what about u ?")
+
+
+
+#retrive the messages 
+
+# Retrieve chat messages for the given chat hash
+messages = mongo_helper_class.get_chat_messages(DATA_BASE_NAME,COLLECTION_NAME,chat_hash)
+for message in messages:
+    if message['sender_hash'] == user_hash_1:
+        print(f'Sender: {message["message"]}')
+    else:
+        print(f'Recipient: {message["message"]}')
 
 
 
--- mongo/mongo_helper.py
+++ mongo/mongo_helper.py
@@ -1,6 +1,7 @@
 #imports 
 from pymongo import MongoClient
 import subprocess
+from datetime import datetime
 
 
 #const values
@@ -240,6 +241,40 @@ class Helper_fun():
         mongo_client.drop_database(db_name)
 
         print("The database has been deleted")
+
+
+    def insert_message_data(self,db_name,collection_name,chat_hash,sender_hash, recipient_hash, message):
+        """
+        The funciton to insert the message in the database and collection
+        """
+        db = mongo_client[db_name]
+        collection = db[collection_name]
+
+        message_doc = {
+        'chat_hash': chat_hash,
+        'sender_hash': sender_hash,
+        'recipient_hash': recipient_hash,
+        'message': message,
+        'timestamp': datetime.now()
+        }
+
+        if collection.insert_one(message_doc):
+            print("Succesfull insertion")
+        else:
+            print("failed to insert message")
+    
+    def get_chat_messages(self,db_name,collection_name,chat_hash):
+        """
+        The function to get the messages from the database
+        """
+
+        db = mongo_client[db_name]
+        collection = db[collection_name]
+
+
+        return collection.find({'chat_hash': chat_hash}).sort('timestamp', 1)
+
+
 
     def delete_data_all(self,db_name,collection_name):
         """
