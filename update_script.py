235e98d7ab9ef83a5941742485a6acaa718399c5
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-07-01 14:45:29 -0400

    redis recovery function code added

--- update_script.py
+++ update_script.py
@@ -66,9 +66,6 @@ os.chdir(original_directory)
 
 redis_recovery_process = subprocess.run('ps aux | grep redis_recovery_func.py', shell=True, capture_output=True, text=True)
 
-#get the pid
-print(redis_recovery_process.stdout)
-
 redis_recovery_process_pid = redis_recovery_process.stdout[11:16]
 
 print(redis_recovery_process_pid)
