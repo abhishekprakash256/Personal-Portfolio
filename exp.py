b4a17839a64cd72d16d330c4b74f85bf2a9cd41d
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-08-06 15:41:13 -0400

    database code

--- exp.py
+++ exp.py
@@ -1,18 +1,32 @@
 
-import subprocess
+import redis
 import os
 
 
-redis_recovery_process = subprocess.run('ps aux | grep redis_recovery_func.py', shell=True, capture_output=True, text=True)
+redis_host = os.getenv('REDIS_HOST', 'localhost')  # Use 'localhost' or the Docker host IP
+redis_port = int(os.getenv('REDIS_PORT', 6379))
 
-#get the pid
-print(redis_recovery_process.stdout)
+redis_client = redis.Redis(host=redis_host, port=redis_port)
 
-redis_recovery_process_pid = redis_recovery_process.stdout[11:16]
 
-print(redis_recovery_process_pid)
+print(redis_client)
 
-#kill the recovery process process
-kill_redis_recovery_process = subprocess.run(['kill', redis_recovery_process_pid], capture_output=True, text=True)
 
-print(kill_redis_recovery_process)
+from pymongo import MongoClient
+import os
+
+mongo_host = os.getenv('MONGO_HOST', 'localhost')  # Use 'localhost' or the Docker host IP
+mongo_port = int(os.getenv('MONGO_PORT', 27017))
+
+
+mongo_client = MongoClient(mongo_host, mongo_port)
+
+try:
+    # Create a MongoDB client
+    client = MongoClient(mongo_host, mongo_port)
+    print("MongoDB client created successfully.")
+except Exception as e:
+    print(f"Error creating MongoDB client: {e}")
+    
+
+
