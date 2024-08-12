c8c0523fd831367cab2ba7fc7a052015bdcebdd6
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-08-12 15:57:58 -0400

    docker testing

---
+++ docker-testing/app.py
@@ -0,0 +1,14 @@
+"""
+make the dummy flask app
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
